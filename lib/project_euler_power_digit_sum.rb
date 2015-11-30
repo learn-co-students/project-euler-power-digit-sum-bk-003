6b843b45d597084a97798ad62d4b21c95809b04e
cloudleo <l.castillo1989@hotmail.com>
2015-11-29 20:24:11 -0500

    gimme more

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,11 @@
 # code your solution here
+require "pry"
+def power_digit_sum(base,exp)
+  sum = base**exp
+  container = []
+  sum.to_s.split("").each {|i| container << i.to_i}
+  answer = container.inject(:+)
+ 
+  return answer
+
+  end
